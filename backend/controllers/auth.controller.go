46a00b423452ab9b670b28b17258e6114a67aa6c
Kei-K23 <keiksl2301@gmail.com>
2024-11-22 15:22:30 +0630

    feat(backend): setup register and login controllers

---
+++ backend/controllers/auth.controller.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/Kei-K23/nextjs-go-auth/models"
+	"github.com/Kei-K23/nextjs-go-auth/services"
+	"github.com/gin-gonic/gin"
+)
+
+func Register(c *gin.Context) {
+	var user models.User
+
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := services.Register(&user); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{"message": "User registered successfully"})
+}
+
+func Login(c *gin.Context) {
+	var input struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	token, err := services.Login(input.Email, input.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User registered successfully", "access_token": token})
+}
