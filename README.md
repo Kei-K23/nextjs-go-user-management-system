04a1455b588d2a52fc95868c3b1eaa3ad7a3e8fc
Kei-K23 <keiksl2301@gmail.com>
2024-11-22 20:10:58 +0630

    feat(backend): add user management CRUD operations

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # NextJS-Go-Auth
 
 Building a complete, high-performance, fully-feature-rich JWT authentication and authorization system with **Go (Gin framework)** and **NextJS**
+
+I building user management system backend api in Golang with including user authentication with JWT token and refresh token function, and CRUD operation for user with role-based access control system. Please write and build a beautiful, minimalist, responsive user management system frontend application using NextJS 15, TypeScript, ShadcnUI, TailwindCSS.
---
+++ backend/services/user.service.go
@@ -0,0 +1,106 @@
+package services
+
+import (
+	"time"
+
+	"github.com/Kei-K23/nextjs-go-auth/database"
+	"github.com/Kei-K23/nextjs-go-auth/models"
+	"github.com/Kei-K23/nextjs-go-auth/utils"
+)
+
+func CreateUser(user *models.User) error {
+	// Hash Password
+	hashedPassword, err := utils.HashPassword(user.Password)
+	if err != nil {
+		return err
+	}
+
+	user.Password = hashedPassword
+	user.CreatedAt = time.Now()
+	user.Status = "Inactive" // By default, all user account inactive
+
+	if err := database.DB.Create(user).Error; err != nil {
+		return err
+	}
+	// Success, then return nill
+	return nil
+}
+
+func GetUserById(id string) (*models.User, error) {
+	var user models.User
+
+	if err := database.DB.Where("id = ?", id).Find(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func GetAllUsers() (*[]models.User, error) {
+	var user []models.User
+
+	if err := database.DB.Find(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func UpdateUser(id string, updateUser *models.User) (*models.User, error) {
+	var user models.User
+	if err := database.DB.Where("id = ?", id).Find(&user).Error; err != nil {
+		return nil, err
+	}
+
+	// Update the password
+	if updateUser.Password != "" {
+		hashedPassword, err := utils.HashPassword(updateUser.Password)
+		if err != nil {
+			return nil, err
+		}
+
+		user.Password = hashedPassword
+	}
+
+	// Update user name
+	if updateUser.Username != "" {
+		user.Username = updateUser.Username
+	}
+
+	// Update user email
+	if updateUser.Email != "" {
+		user.Email = updateUser.Email
+	}
+
+	// Update user status
+	if updateUser.Status != "" {
+		user.Status = updateUser.Status
+	}
+
+	// Update user role
+	if updateUser.Role != "" {
+		user.Role = updateUser.Role
+	}
+
+	user.UpdatedAt = time.Now()
+	// Save changes to db
+	if err := database.DB.Save(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func DeleteUser(id string) error {
+	var user models.User
+	if err := database.DB.Where("id = ?", id).Find(&user).Error; err != nil {
+		return err
+	}
+
+	// Delete
+	if err := database.DB.Delete(&user).Error; err != nil {
+		return err
+	}
+
+	return nil
+}
