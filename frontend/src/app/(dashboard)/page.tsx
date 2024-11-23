4bc76cc18e5d0ae500f4e6c410550751c82aa2dc
Kei-K23 <keiksl2301@gmail.com>
2024-11-23 17:07:36 +0630

    feat(frontend): add header component for dashboard layout

--- frontend/src/app/(dashboard)/page.tsx
+++ frontend/src/app/(dashboard)/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { useAuth } from "@/features/auth/hooks/use-auth";
 import useProtectRoute from "@/features/auth/hooks/use-protect-route";
 import { columns } from "@/features/users/components/user-table/columns";
 import { UserDataTable } from "@/features/users/components/user-table/data-table";
@@ -9,21 +8,15 @@ import Link from "next/link";
 
 export default function DashboardPage() {
   useProtectRoute();
-  const { authUser, logout } = useAuth();
   const { users, isLoadingUsers } = useUsers();
 
   return (
     <div className="space-y-4">
-      <div>
-        <h3>{authUser?.username}</h3>
-        <h3>{authUser?.email}</h3>
-      </div>
       <div className="flex justify-between items-center">
         <h1 className="text-2xl font-bold">User Management</h1>
         <Link href="/users/new">
           <Button>Create User</Button>
         </Link>
-        <Button onClick={logout}>Logout</Button>
       </div>
       {isLoadingUsers ? (
         <p>loading...</p>
---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { useAuth } from "@/features/auth/hooks/use-auth";
+import Link from "next/link";
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { LogOut, User } from "lucide-react";
+import { Button } from "./ui/button";
+
+export default function Header() {
+  const { authUser, logout } = useAuth();
+  return (
+    <header className="border-b">
+      <div className="flex items-center justify-between max-w-7xl mx-auto py-2">
+        <Link href={"/"}>UMS</Link>
+        <div className="flex items-center gap-3">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                size={"sm"}
+                variant={"ghost"}
+                className="focus-visible:ring-0"
+              >
+                <Avatar className="cursor-pointer size-8">
+                  <AvatarImage src="" />
+                  <AvatarFallback className="text-xl text-white font-bold bg-black dark:bg-white dark:text-black">
+                    {authUser?.username?.charAt(0).toUpperCase()}
+                  </AvatarFallback>
+                </Avatar>
+                <div className="flex flex-col items-start">
+                  <h3>{authUser?.username}</h3>
+                  <p className="text-muted-foreground">{authUser?.email}</p>
+                </div>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-[180px]">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem className="cursor-pointer">
+                <User className="size-5" />
+                Profile
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                onClick={logout}
+                className="cursor-pointer bg-destructive text-destructive-foreground focus:bg-destructive/90 focus:text-white"
+              >
+                <LogOut className="size-5" />
+                Logout
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+    </header>
+  );
+}
